130934415f5d0aa342e6798aefca1856e3650fcb
Jeremy Cloutier <jcloutier24@yahoo.com>
2016-01-05 15:02:35 -0500

    updated app to manipulate class on parent element

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
-"use strict";
+//"use strict";
+
 var data = {
   "coord": {
     "lon": -78.96,
@@ -53,6 +54,53 @@ var apiKey = '9eed8518c4e923f4c9d88217fe9c998b',
       "extreme": "http://unsplash.it/1680/1050/?image=536",
       "additional": "http://unsplash.it/1680/1050/?image=459"
     },
+    conditionCodes = [
+      {
+        lowerBound: 200,
+        upperBound: 299,
+        condition: 'thunderstorm'
+      },
+      {
+        lowerBound: 300,
+        upperBound: 399,
+        condition: 'drizzle'
+      },
+      {
+        lowerBound: 400,
+        upperBound: 599,
+        condition: 'rain'
+      },
+      {
+        lowerBound: 600,
+        upperBound: 699,
+        condition: 'snow'
+      },
+      {
+        lowerBound: 700,
+        upperBound: 799,
+        condition: 'atmosphere'
+      },
+      {
+        lowerBound: 800,
+        upperBound: 800,
+        condition: 'clear'
+      },
+      {
+        lowerBound: 801,
+        upperBound: 899,
+        condition: 'clouds'
+      },
+      {
+        lowerBound: 900,
+        upperBound: 909,
+        condition: 'extreme'
+      },
+      {
+        lowerBound: 951,
+        upperBound: 962,
+        condition: 'additional'
+      }
+    ],
     windDirTranslations = {
       '0': 'N',
       '22.5': 'NNE',
@@ -72,31 +120,6 @@ var apiKey = '9eed8518c4e923f4c9d88217fe9c998b',
       '337.5': 'NNW',
       '360': 'N',
     };
-
-$(document).ready(function() {
-  var $toggles = $('.weather-toggle');
-  var $html = $('html');
-  var $imageChecker = $('.image-checker');
-
-  $toggles.click(function() {
-    var val = $(this).val();
-    $html.attr('class', '');
-
-    if (val !== 'none') {
-      $html.attr('class', val).waitForImages({
-        each: function() {
-          //$html.attr('class', val);
-        },
-        finished: function() {
-          $html.addClass('loaded');
-        },
-        waitForAll: true,
-      })
-    } else {
-      $html.attr('class', 'unloaded');
-    }
-  });
-});
 
 var WeatherDetail = React.createClass({
   render: function() {
@@ -207,6 +230,7 @@ var TemperatureContainer = React.createClass({
 var Weather = React.createClass({
   getInitialState: function() {
     return {
+      status: 'loading',
       geo: false,
       measurementSystem: 'imperial',
       data: []
@@ -215,43 +239,59 @@ var Weather = React.createClass({
   handleMeasurementSystemChange: function(system) {
     this.setState({measurementSystem: system});
   },
+  resolveConditionCode: function(code) {
+    for (var i = 0; i < conditionCodes.length; i++) {
+      if (code >= conditionCodes[i].lowerBound && code <= conditionCodes[i].upperBound) {
+        return conditionCodes[i].condition;
+      }
+    }
+  },
   loadWeather: function(lat, long) {
-    this.setState({geo: true, data: data});
 
-    // var url = this.props.api
-    //   +'&lat='+lat
-    //   +'&lon='+long;
-    //
-    // $.ajax({
-    //   url: url,
-    //   dataType: 'json',
-    //   cache: false,
-    //   success: (data) => {
-    //     console.log(data);
-    //     this.setState({geo: true, data: data});
-    //     console.log(this.state);
-    //   },
-    //   error: (xhr, status, err) => {
-    //     console.error(this.props.api, status, err.toString());
-    //   }
-    //
-    // });
+
+    var url = this.props.api
+      +'&lat='+lat
+      +'&lon='+long;
+
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      cache: false,
+      success: (data) => {
+        this.setState({status: 'loaded', data: data});
+        var $node = $(this.refs.weatherContainer.parentNode);
+        var condition = this.resolveConditionCode(this.state.data.weather[0].id);
+
+        $node.attr('class', condition).waitForImages({
+          finished: function() {
+            $node.addClass('loaded');
+          },
+          waitForAll: true,
+        });
+      },
+      error: (xhr, status, err) => {
+        this.setState({status: 'error'});
+        console.error(this.props.api, status, err.toString());
+      }
+
+    });
   },
   componentDidMount: function() {
-    console.log('mounted');
     if ('geolocation' in navigator) {
-
       navigator.geolocation.getCurrentPosition((position) => {
         this.loadWeather(position.coords.latitude, position.coords.longitude);
+      }, () => {
+        this.setState({status: 'error'});
+        console.log('error', 'Unable to retrieve location');
       });
     } else {
-      this.setState({geo: false});
+      this.setState({status: 'error'});
     }
   },
   render: function() {
-    if(this.state.geo !== false) {
+    if(this.state.status === 'loaded') {
       return (
-        <div className="weather">
+        <div className="weather" ref="weatherContainer">
           <TemperatureContainer
               temp={this.state.data.main.temp}
               location={this.state.data.name}
@@ -262,10 +302,20 @@ var Weather = React.createClass({
               measurementSystem={this.state.measurementSystem}  />
         </div>
       );
+    } else if (this.state.status === 'loading') {
+      return (
+        <div className="message loading">
+          <p>Loading</p>
+          <svg className="loading-icon"  x="0" y="0" viewBox="0 0 50 50">
+            <path d="M46.6 29h3C47.8 40.9 37.5 50 25 50S2.2 40.9 0.3 29h3C5.2 39.2 14.2 47 25 47S44.8 39.2 46.6 29z"/>
+            <path d="M3.4 21h-3C2.2 9.1 12.5 0 25 0s22.8 9.1 24.7 21h-3C44.8 10.8 35.8 3 25 3S5.2 10.8 3.4 21z"/>
+          </svg>
+        </div>
+      );
     } else {
       return (
-        <div className="Error">
-          <h1>Loading</h1>
+        <div className="message error">
+          <p>Your browser does not support geolocation</p>
         </div>
       );
     }
